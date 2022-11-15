63d65a1274dc58e77c13a4cc2c086118e1da8fa6
Mubeen Ali <cmubeenali@gmail.com>
2022-11-15 21:16:53 +0530

    returned to bruteforce way

--- two-sum.py
+++ two-sum.py
@@ -6,43 +6,30 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
-        """        
+        """     
         result=[]
-        d_nums={}
-        i=0
-        for x in nums:
-            if (x < target):
-                d_nums[i]=x
-            i+=1
-
-        half_of_target=round(target/2)
-        
-        arr_right={}
-        arr_left={}
-
-        for index in d_nums:
-            if d_nums[index]>half_of_target:
-                arr_right[index]=d_nums[index]
-            else:
-                arr_left[index]=d_nums[index]
-                
-        is_found=False
-        for r in arr_right:
-            for l in arr_left:
-                if (target-arr_right[r])==arr_left[l]:
-                    result.append(l)
-                    result.append(r)
-                    is_found=True
-                    break
+        idx_x=0
+        for x in nums:            
+            is_found=False
+            if x<=target:
+                idx_y=0
+                for y in nums:
+                    if y<=target and idx_x!=idx_y:
+                        if x+y==target:
+                            result.append(idx_x)
+                            result.append(idx_y)
+                            is_found=True
+                            break
+                    idx_y+=1
+            idx_x+=1
             if is_found:
                 break
-
         return result
 
 if __name__=='__main__':
     start_time=datetime.now()
     two_sum=Solution()
-    result= two_sum.twoSum([2,7,11,15],9)
+    result= two_sum.twoSum([0,4,3,0],0)
     end_time=datetime.now()
     print(result)
     elapsed_time=end_time-start_time
