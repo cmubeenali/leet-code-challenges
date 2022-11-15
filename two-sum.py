04e3e96f0283ab9616731597c81d69c10eb56886
Mubeen Ali <cmubeenali@gmail.com>
2022-11-15 16:49:33 +0530

    changes

--- two-sum.py
+++ two-sum.py
@@ -1,32 +1,27 @@
+import time
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
-        """
+        """        
         result=[]
-        idx_x=0
-        for x in nums:            
-            is_found=False
-            if x<target:
-                idx_y=0
-                for y in nums:
-                    if y<target and idx_x!=idx_y:
-                        if x+y==target:
-                            result.append(idx_x)
-                            result.append(idx_y)
-                            is_found=True
-                            break
-                    idx_y+=1
-            idx_x+=1
-            if is_found:
-                break
+        d_nums=[]
+        i=0
+        for x in nums:
+            d_nums.append({i:x,'is_excluded':(True if x >= target else False)})
+            i+=1
+
+        # d_nums[:]=
+
         return result
 
 if __name__=='__main__':
+    start_time=time.time()
     two_sum=Solution()
-    nums=[2,7,11,15]
-    target=9
-    result= two_sum.twoSum(nums,target)
+    result= two_sum.twoSum([2,11,15,7],9)
+    end_time=time.time()
+    print((start_time-end_time))
     print(result)
