b31ca008d07ab8b59cde7cfc757bc820bd7ca4a6
Mubeen Ali <cmubeenali@gmail.com>
2022-11-15 21:21:20 +0530

    solution from leet. i fucked up 😔

--- two-sum.py
+++ two-sum.py
@@ -1,36 +1,12 @@
-from datetime import datetime
+from ast import List
 
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """     
-        result=[]
-        idx_x=0
-        for x in nums:            
-            is_found=False
-            if x<=target:
-                idx_y=0
-                for y in nums:
-                    if y<=target and idx_x!=idx_y:
-                        if x+y==target:
-                            result.append(idx_x)
-                            result.append(idx_y)
-                            is_found=True
-                            break
-                    idx_y+=1
-            idx_x+=1
-            if is_found:
-                break
-        return result
 
-if __name__=='__main__':
-    start_time=datetime.now()
-    two_sum=Solution()
-    result= two_sum.twoSum([0,4,3,0],0)
-    end_time=datetime.now()
-    print(result)
-    elapsed_time=end_time-start_time
-    print('Execution time is '+str(elapsed_time)+' seconds')
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]] 
